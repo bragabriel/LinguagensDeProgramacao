19d835aaf740434b17c11caaead2ecb8e704f3c2
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2021-11-22 22:03:00 -0300

    Update file
    Update 'TrabalhandoComStrings.c'

--- C/Conceitos/Strings/TrabalhandoComStrings.c
+++ C/Conceitos/Strings/TrabalhandoComStrings.c
@@ -9,48 +9,47 @@ Se isso acontecer, imprime 'true', se não, imprime 'false'
 #include <ctype.h>
 
 int StringChallenge(char * str) {
+   
+	int i=0, tam;
   
-  // code goes here  
-  int i=0, tam;
-  
-  tam = strlen(str);
+	tam = strlen(str);
 
-  char aux[tam];
+	char aux[tam];
 
-  strcpy(aux, str);
+	strcpy(aux, str);
   
-  //Colocando as strings em Maiusculo
-  for(i=0; i<tam; i++){
-    str[i] = toupper(str[i]);
-    aux[i] = toupper(aux[i]);
-  }
+	//Colocando as strings em Maiusculo
+	for(i=0; i<tam; i++){
+		str[i] = toupper(str[i]);
+		aux[i] = toupper(aux[i]);
+	}
   
-  for(i=0; i<tam; i++){
-      if(str[i]=='A'){
-        if(aux[i+3]=='B'){
-          //printf("true");
-          return(1);
-
-        }
-      }
-    }
-  return(0);
-  //str = "false";
-  //printf("false");
+	for(i=0; i<tam; i++){
+		if(str[i]=='A'){
+    		if(aux[i+3]=='B'){
+        		//printf("true");
+        		return(1);
+    		}
+    	}
+	}
+	
+	return(0);
 }
 
 int main(void) { 
-  int res;
-  char str[50] = "Laura sobs";
-  // keep this function call here
-  res = StringChallenge(str);
 
- if(res==1){
-    printf("true");
-  }
-  else{
-    printf("false");
-  }
-  return 0;
-    
+	int res;
+	char str[50] = "Laura sobs"; //Deve retornar TRUE
+	//char str[50] = "Gabriel"; //Deve retornar FALSE
+
+	res = StringChallenge(str);
+
+	if(res==1){
+    	printf("true");
+	}
+	else{
+		printf("false");
+	}
+  
+  return 0;  
 }
