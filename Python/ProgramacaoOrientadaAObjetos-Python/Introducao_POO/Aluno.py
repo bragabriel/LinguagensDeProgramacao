ea0f62b43b2d8c630449fc2a2fc0c1a730b4a532
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2022-01-04 22:36:22 -0300

    Update file - Problem solved

--- Python/ProgramacaoOrientadaAObjetos-Python/Introducao_POO/Aluno.py
+++ Python/ProgramacaoOrientadaAObjetos-Python/Introducao_POO/Aluno.py
@@ -1,66 +1,70 @@
 #Classe:
 class Aluno():
+    nome = ''
+    ra = ''
     notaA1 = 0
     notaA2 = 0
     media = 0
     frequencia = 0
         
-#Métodos:
-def set_nome(self, nome):
-    #Condicionando o NOME à ser uma String
-    if(type(nome)==type(str())):
-        self.__nome = nome
-    else:
-        print('Nome deve ser uma String!')
-    
-def get_nome(self):
-    return self.__nome
-    
-def set_ra(self, ra):
-    #Condicionando o R.A à ser um INT
-    if(type(ra)==type(int())):
-        self.__ra =ra
-    else:
-        print('R.A deve ser um Número Inteiro!')
+    #Métodos:
+    def set_nome(self, nome):
+        #Condicionando o NOME à ser uma String
+        print('Método setter sendo utilizado')
+        if(type(nome)==type(str())):
+            self.__nome = nome
+        else:
+            print('Nome deve ser uma String!')
         
-def set_notaA1(self, notaA1):
-    self.notaA1 = notaA1
-    
-    
-def set_notaA2(self, notaA2):
-    self.notaA2 = notaA2
-    
-def calcular_Media(self):
-    self.media = (self.notaA1 + (2*self.notaA2)) / 3
+    def get_nome(self):
+        print('Método getter sendo utilizado')
+        return self.__nome
+        
+    def set_ra(self, ra):
+        #Condicionando o R.A à ser um INT
+        if(type(ra)==type(int())):
+            self.__ra =ra
+        else:
+            print('R.A deve ser um Número Inteiro!')
+            
+    def set_notaA1(self, notaA1):
+        self.notaA1 = notaA1
+        
+        
+    def set_notaA2(self, notaA2):
+        self.notaA2 = notaA2
+        
+    def calcular_Media(self):
+        self.media = (self.notaA1 + (2*self.notaA2)) / 3
 
-def set_frequencia(self, freq):
-    self.frequencia = freq
-    
-def get_media(self):
-    print('A média do aluno ', self._aluno, 'é: ', self.media)
-    
-def situacao_Aluno(self):
-    if(self.frequencia < 75):
-        print('Aluno está reprovado por faltas!')
+    def set_frequencia(self, freq):
+        self.frequencia = freq
         
-    elif(self.media >= 5):
-        print('Aluno está reprovado!')
+    def get_media(self):
+        print(f'A média do aluno {self.__nome} é: {self.media}')
         
-    elif(self.media >=3):
-        print('Aluno está de RE!')
+    def situacao_Aluno(self):
+        if(self.frequencia < 75):
+            print('Aluno está Reprovado por faltas!')
+            
+        elif(self.media >= 5):
+            print('Aluno está Aprovado! Parabéns!')
+            
+        elif(self.media >=3):
+            print('Aluno está de RE!')
+            
+        else:
+            print('Aluno está Reprovado por nota!')
+            
+    def getDadosAluno(self):
+        print('Aluno:', self.__nome)
+        print('R.A:', self.__ra)
+        print('Frequência:', self.frequencia)
+        print('Nota A1:', self.notaA2)
+        print('Nota A2:', self.notaA2)
+        print('Média:', self.media)
         
-    else:
-        print('Aluno está reprovado por nota!')
         
-def getDadosAluno(self):
-    print('\nAluno: ', self._nome)
-    print('R.A: ', self._ra)
-    print('Frequência: ', self.frequencia)
-    print('Nota A1: ', self.notaA2)
-    print('Nota A2: ', self.notaA2)
-    print('Média: ', self.media)
-    
-    
     #Atributo PRIVADO:
-    nome = property(set_nome)
-    ra = property(set_ra)
+    # nome = property(set_nome)
+    # ra = property(set_ra)
--- Python/ProgramacaoOrientadaAObjetos-Python/Introducao_POO/Main.py
+++ Python/ProgramacaoOrientadaAObjetos-Python/Introducao_POO/Main.py
@@ -1,5 +1,62 @@
-from aluno import *
+from aluno import Aluno
 
 Aluno1 = Aluno()
+Aluno2 = Aluno()
+Aluno3 = Aluno()
 
-Aluno1.
+print()
+print('=-='*10)
+print('Aluno 1:')
+
+Aluno1.set_nome('Gabriel Braga')
+Aluno1.set_ra(110080)
+Aluno1.set_notaA1(10)
+Aluno1.set_notaA2(10)
+Aluno1.set_frequencia(100)
+Aluno1.calcular_Media()
+print('get_nome: ', Aluno1.get_nome())
+Aluno1.get_media()
+print('\nDados do aluno:')
+Aluno1.getDadosAluno()
+print('\nSituação do aluno:', end=' ')
+Aluno1.situacao_Aluno()
+
+print('=-='*10, '\n')
+
+
+print('=-='*10)
+print('Aluno 2:')
+
+Aluno2.set_nome('Douglas')
+Aluno2.set_ra(564232)
+Aluno2.set_notaA1(4)
+Aluno2.set_notaA2(4)
+Aluno2.set_frequencia(80)
+Aluno2.calcular_Media()
+print('get_nome: ', Aluno1.get_nome())
+Aluno2.get_media()
+print('\nDados do aluno:')
+Aluno2.getDadosAluno()
+print('\nSituação do aluno:', end=' ')
+Aluno2.situacao_Aluno()
+
+print('=-='*10, '\n')
+
+
+print('=-='*10)
+print('Aluno 3:')
+
+Aluno3.set_nome('Geraldo')
+Aluno3.set_ra(458789)
+Aluno3.set_notaA1(7)
+Aluno3.set_notaA2(6)
+Aluno3.set_frequencia(20)
+Aluno3.calcular_Media()
+print('get_nome: ', Aluno3.get_nome())
+Aluno3.get_media()
+print('\nDados do aluno:')
+Aluno3.getDadosAluno()
+print('\nSituação do aluno:', end=' ')
+Aluno3.situacao_Aluno()
+
+print('=-='*10, '\n')
