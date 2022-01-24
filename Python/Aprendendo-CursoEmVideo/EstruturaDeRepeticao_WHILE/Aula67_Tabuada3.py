3ea8f32caed255e89c348a95a7e401f24a118f9b
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2022-01-23 22:08:52 -0300

    Add file
    Python file

---
+++ Python/Aprendendo-CursoEmVideo/EstruturaDeRepeticao_WHILE/Aula67_Tabuada3.py
@@ -0,0 +1,19 @@
+from os import name
+
+
+n = 1
+print('\nDigite um número negativo para encerrar o programa!\n')
+
+while True:
+
+    print('-' * 40)
+    n = int(input('Quer ver a tabuada de qual valor? '))
+    print('-' * 40)
+    
+    if(n < 0):
+        break
+    
+    for i in range(1, 11):
+        print(f'{n} x {i} = {n*i}')
+        
+print('Programa encerrado.')
