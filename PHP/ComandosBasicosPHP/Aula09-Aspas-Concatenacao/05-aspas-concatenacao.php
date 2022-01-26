781ad256b717f6c391a4eb46a10a232d8e685f58
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2022-01-26 14:55:35 -0300

    Add file
    PHP file

---
+++ PHP/ComandosBasicosPHP/Aula09-Aspas-Concatenacao/05-aspas-concatenacao.php
@@ -0,0 +1,12 @@
+<?php
+
+$nome = 'Gabriel Braga';
+
+
+echo '<br> Teste 01: Meu nome é $nome';
+
+echo '<br> Teste 02: Meu nome é '.$nome.' e minha idade é \'20\''; //Aspas simples são Iterais (Considera tudo entre aspas simples como: texto)
+                                                                    //Por isso para mostrar uma variável, precisamos concatenar.
+echo "<br> Teste 03: Meu nome é $nome e minha idade é \"20\""; //Aspas duplas são Interpretativas
+
+?>
