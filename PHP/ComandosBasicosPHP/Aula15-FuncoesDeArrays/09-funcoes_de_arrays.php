73e597ad395a89c502a2e08020d2312323768cbb
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2022-01-27 13:58:09 -0300

    Update 09-funcoes_de_arrays.php
    Update PHP file

--- PHP/ComandosBasicosPHP/Aula15-FuncoesDeArrays/09-funcoes_de_arrays.php
+++ PHP/ComandosBasicosPHP/Aula15-FuncoesDeArrays/09-funcoes_de_arrays.php
@@ -13,7 +13,7 @@ else{
     echo "Não é um array!";
 }
 
-echo "<br><hr>";
+echo "<br><br><hr>";
 //---------------------------------
 // in_array("valor", ($array)); //Verifica se um determinado valor existe em alguma posição do array
 
@@ -21,7 +21,7 @@ echo in_array("Gabriel", $nomes); //Verifica se um determinado valor existe em a
 
 echo in_array("Carlos", $nomes); //Não retorna nada se o valor for Falso (se não tiver o valor no array)
 
-echo "<br><hr>";
+echo "<br><br><hr>";
 
 if(in_array("Elza", $nomes)){
     echo "Existe no array!";
@@ -31,7 +31,7 @@ else{
 }
 
 
-echo "<br><hr>";
+echo "<br><br><hr>";
 //---------------------------------
 // array_keys($array) = Retorna um novo array com as chaves (ÍNDICES) do array passado como parâmetro
 echo("Array \$nomes: ");
@@ -54,7 +54,7 @@ echo("Array \$keys2: ");
 print_r($keys2);
 
 
-echo "<br><hr>";
+echo "<br><br><hr>";
 //---------------------------------
 // array_values($array) = Retorna um novo array com os valores do array passado como parâmetro
 echo("Array \$nomes: ");
@@ -65,7 +65,7 @@ echo("Array \$<ins>VALUES</ins>: ");
 print_r($values);
 
 
-echo "<br><hr>";
+echo "<br><br><hr>";
 //---------------------------------
 // array_merge($array1, $array2) = Agrega o conteúdo de dois arrays
 $carros = array("Camaro", "Uno", "Gol");
@@ -80,11 +80,11 @@ print_r($motos);
 echo "<br>";
 
 $veiculos = array_merge($carros, $motos);
-echo("Array <ins>MERGE</ins>\$veiculos: ");
+echo("Array <ins>MERGE</ins> \$veiculos: ");
 print_r($veiculos);
 
 
-echo "<br><hr>";
+echo "<br><br><hr>";
 //---------------------------------
 // array_pop($array) = Exclui a ÚLTIMA posição do array
 echo("Array \$carros: ");
@@ -94,10 +94,9 @@ echo "<br>";
 echo("Array <ins>POP</ins> \$carros: ");
 array_pop($carros);
 print_r($carros);
-echo "<br>";
 
 
-echo "<br><hr>";
+echo "<br><br><hr>";
 //---------------------------------
 // array_shift($array) = Exclui a PRIMEIRA posição do array
 echo("Array \$carros: ");
@@ -107,10 +106,9 @@ echo "<br>";
 echo("Array <ins>SHIFT</ins> \$carros: ");
 array_shift($carros);
 print_r($carros);
-echo "<br>";
 
 
-echo "<br><hr>";
+echo "<br><br><hr>";
 //---------------------------------
 // array_unshift($array, "valor1", "valor2") = Adiciona um ou mais elementos no INÍCIO do array
 $frutas = array("Uva", "Laranja", "Maçã");
@@ -121,10 +119,9 @@ echo "<br>";
 echo("Array <ins>UNSHIFT</ins> \$frutas: ");
 array_unshift($frutas, "Manga", "Acerola", "Morango");
 print_r($frutas);
-echo "<br>";
 
 
-echo "<br><hr>";
+echo "<br><br><hr>";
 //---------------------------------
 // array_push($array, "valor1", "valor2") = Adiciona um ou mais elementos no FINAL do array
 $frutas = array("Uva", "Laranja", "Maçã");
@@ -135,10 +132,9 @@ echo "<br>";
 echo("Array <ins>PUSH</ins> \$frutas: ");
 array_push($frutas, "Manga", "Acerola", "Morango");
 print_r($frutas);
-echo "<br>";
 
 
-echo "<br><hr>";
+echo "<br><br><hr>";
 //---------------------------------
 // array_combine($array, "valor1", "valor2") = Mescla dois arrays
 $keys = array("Campeão", "Vice", "Terceiro");
@@ -155,4 +151,22 @@ echo "<br>";
 $times = array_combine($keys, $values);
 echo("Array <ins>COMBINE</ins> \$times: ");
 print_r($times);
+
+
+echo "<br><br><hr>";
+//---------------------------------
+// array_sum() = Calcula a soma dos elementos de um array
+$soma = array(5.5, 6, 10.1, 8);
+echo("Array \$soma: ");
+print_r($soma);
+echo "<br>";
+
+$total = array_sum($soma);
+echo("Array <ins>SUM</ins> \$soma: $total");
+
+
+echo "<br><br><hr>";
+//---------------------------------
+// array_sum() = Calcula a soma dos elementos de um array
+
 ?>
