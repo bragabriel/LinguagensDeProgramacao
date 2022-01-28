d419d915acf20171320137abd65226596ebace4f
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2022-01-28 13:55:12 -0300

    Add file
    PHP file (while, do_while)

---
+++ PHP/ComandosBasicosPHP/Aula23-OperadoresAtribuicao/13-operadores-de-atribuicao.php
@@ -0,0 +1,28 @@
+<?php
+
+$a = 10;
+$b = 5;
+$a += $b;
+echo "$a <br><hr>";
+
+$a = 10;
+$b = 5;
+$a-=$b;
+echo "$a <br><hr>";
+
+$a = 10;
+$b = 5;
+$a*=$b;
+echo "$a <br><hr>";
+
+$a = 10;
+$b = 5;
+$a/=$b;
+echo "$a <br><hr>";
+
+$a = 10;
+$b = 5;
+$a%=$b; //módulo - resto da divisão
+echo "$a <br><hr>";
+
+?>
