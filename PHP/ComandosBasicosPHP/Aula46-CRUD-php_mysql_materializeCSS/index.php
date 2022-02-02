231861a93e1b3fdf76a479e77c03a6fcd90a5412
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2022-02-01 21:28:45 -0300

    Add files
    PHP files (Sistema CRUD -create, read, update, delete- com PHP, Banco de Dados e o Framework Materialize CSS)

---
+++ PHP/ComandosBasicosPHP/Aula46-CRUD-php_mysql_materializeCSS/index.php
@@ -0,0 +1,50 @@
+<?php 
+
+/*************************************/
+/*               CRUD                */
+/*  Create - Read - Update - Delete  */
+/*************************************/
+
+/*  OBS: Utilizando PHP procedural.  */
+
+
+//Incluindo o header
+include_once 'includes/header.php';
+
+
+?>
+
+<div class="row">
+    <div class="col s12 m6 push-m3">
+        <h3 class="light">Clientes</h3>
+        <table class="striped">
+            <thead>
+                <tr>
+                    <th>Nome:</th>
+                    <th>Sobrenome:</th>
+                    <th>Email:</th>
+                    <th>Idade:</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <tr>
+                    <td>Gabriel</td>
+                    <td>Braga</td>
+                    <td>teste@teste.com</td>
+                    <td>20</td>
+                    <td><a href="#" class="btn-floating orange"><i class="material-icons">edit</i></a></td>
+                    <td><a href="#" class="btn-floating red"><i class="material-icons">delete</i></a></td>
+                </tr>
+            </tbody>
+        </table>
+        <br>
+        <a href="" class="btn">ADICIONAR CLIENTE</a>
+    </div>
+</div>
+
+
+<?php 
+    //Incluindo o footer
+    include_once 'includes/footer.php';
+?>
---
+++ PHP/ComandosBasicosPHP/Aula46-CRUD-php_mysql_materializeCSS/php_action/db_connect.php
@@ -0,0 +1,15 @@
+<?php
+
+//Conexão com o Banco de Dados
+$servername = "localhost";
+$username = "root";
+$password = "";
+$db_name = "crud";
+
+$connect = mysqli_connect($servername, $username, $password, $db_name);
+
+if(mysqli_connect_error()){
+    echo "Erro na conexão com o banco: ".mysqli_connect_error();
+}
+
+?>
