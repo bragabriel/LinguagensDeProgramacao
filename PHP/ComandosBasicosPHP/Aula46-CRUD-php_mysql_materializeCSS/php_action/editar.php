7244cece43fb0e79b40d781574c978cc7fd0cb50
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2022-02-02 11:57:11 -0300

    Update editar.php
    Corrigido o problema - Botão 'atualizar' não funcionava

--- PHP/ComandosBasicosPHP/Aula46-CRUD-php_mysql_materializeCSS/php_action/editar.php
+++ PHP/ComandosBasicosPHP/Aula46-CRUD-php_mysql_materializeCSS/php_action/editar.php
@@ -30,7 +30,7 @@ if(isset($_GET['id'])){
 <div class="row">
     <div class="col s12 m6 push-m3">
         <h3 class="light">Editar Cliente</h3>
-        <form action="php_action/update.php" method="POST">
+        <form action="update.php" method="POST">
             <input type="hidden" name="id" value="<?php echo $dados['id'];?>">
             <div class="input-field col s12">
                 <input type="text" name="nome" id="nome" value="<?php echo $dados['nome']?>">
@@ -52,7 +52,7 @@ if(isset($_GET['id'])){
                 <label for="idade">Idade</label>
             </div>
 
-            <button type="submit" name="btn-editar" class="btn">Editar</button>
+            <button type="submit" name="btn-editar" class="btn">Atualizar</button>
             <a href="index.php" class="btn green">Lista de Clientes</a>
         </form>
      
