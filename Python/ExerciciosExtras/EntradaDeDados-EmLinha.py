1324200a1054cc1e397d771857fa7057f1a245fd
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2022-03-20 13:42:55 -0300

    Create EntradaDeDados-EmLinha.py

---
+++ Python/ExerciciosExtras/EntradaDeDados-EmLinha.py
@@ -0,0 +1,16 @@
+# 01: 
+numeros = input().split()
+num1 = int(numeros[0])
+num2 = int(numeros[1])
+num3 = int(numeros[2])
+
+
+# 02: 
+a, b, c = input().split(" ")
+a = int(a)
+b = float(b)
+c = int(c)
+
+
+# 03:
+x, y = map(float, input().split())
