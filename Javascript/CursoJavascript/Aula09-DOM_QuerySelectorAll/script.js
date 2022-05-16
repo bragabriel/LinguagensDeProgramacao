32128063ae480011e086e6bfb24e9eddc4811bb4
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2022-05-16 13:19:54 -0300

    add: Js QuerySelectorAll

---
+++ Javascript/CursoJavascript/Aula09-DOM_QuerySelectorAll/script.js
@@ -0,0 +1,11 @@
+console.log(
+    document.querySelectorAll(".times")
+)
+
+console.log(
+    document.getElementById("futebol-europeu").querySelectorAll(".times")
+)
+
+console.log(
+    document.getElementById("futebol-europeu").querySelectorAll(".times")[0].innerHTML
+)
