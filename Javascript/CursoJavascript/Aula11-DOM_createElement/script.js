42c2e6ab705bd9dde7d4fd4470cd145c0be5bb5e
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2022-05-16 13:20:37 -0300

    add: js create element

---
+++ Javascript/CursoJavascript/Aula11-DOM_createElement/script.js
@@ -0,0 +1,18 @@
+console.log(
+    document.createElement("div")
+)
+
+var imagem = document.createElement("img")
+imagem.src = "img.png"
+
+console.log(
+    imagem
+)
+
+
+var paragrafo = document.createElement("p")
+paragrafo.innerHTML = "Olá Mundo!"
+
+console.log(
+    paragrafo
+)
