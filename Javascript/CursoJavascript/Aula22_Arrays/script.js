bf6824176a8c4e1862df4ac729b959ee9dc8f231
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2022-05-17 11:58:06 -0300

    Add: Js Array

---
+++ Javascript/CursoJavascript/Aula22_Arrays/script.js
@@ -0,0 +1,24 @@
+/* Array */
+// Estrutura de dados que armazena uma coleção de elementos
+
+//Array = []
+//No array, temos valor: ['memoria', 500, 'teclado']
+
+
+//Objeto = {}
+//No obj, temos índices e valor: {memoria: '500'}
+
+let meuArray = ['computador', 'teclado', 1500.00, {memoria: '16gb', hd:'1tb'}]
+console.log(meuArray)
+console.log(meuArray.length)
+console.log(meuArray[1])
+console.log(meuArray[3].memoria)
+console.log(meuArray[3].hd)
+
+let meuArray2 = [
+    'notebook',
+    ['dell', 'samsung', 'hp']
+]
+console.log(meuArray2[1])
+console.log(meuArray2[1][2])
+
