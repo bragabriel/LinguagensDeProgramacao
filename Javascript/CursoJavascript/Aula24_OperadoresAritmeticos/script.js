72ca1fbfaee67cc644fc02f09826d73c868abfae
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2022-05-18 15:40:10 -0300

    Add: Js Operadores

---
+++ Javascript/CursoJavascript/Aula24_OperadoresAritmeticos/script.js
@@ -0,0 +1,11 @@
+/* Operadores Aritméticos */
+
+let x = 10
+let y = 5
+
+console.log(x+y)
+console.log(x-y)
+console.log(x/y)
+console.log(x*y)
+console.log(x**y) //elevado
+console.log(x%y) //mod
---
+++ Javascript/CursoJavascript/Aula25_OperadoresAtribuicao/script.js
@@ -0,0 +1,13 @@
+/* Operadores de Atribuição */
+
+let x = 10
+let y = 5
+
+x = x+y
+
+y += x
+
+x++;
+
+console.log(x)
+console.log(y)
