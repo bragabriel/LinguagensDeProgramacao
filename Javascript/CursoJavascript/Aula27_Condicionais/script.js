8d2092b2d2d7460aeb26095765297235ac069aba
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2022-05-20 11:08:15 -0300

    Add: Js Condicionais

---
+++ Javascript/CursoJavascript/Aula27_Condicionais/script.js
@@ -0,0 +1,19 @@
+/* Condicionais */ 
+
+let pais = "Brasil"
+let estado = "São Paulo"
+
+if(pais == "Brasil"){
+    console.log("Você é brasileiro")
+}else{
+    console.log("Você não é brasileiro")
+}
+
+switch(estado){
+    case 'São Paulo':
+        console.log("Você é paulista")
+        break
+    case 'Rio de Janeiro':
+        console.log("Você é carioca")
+        break
+}    
