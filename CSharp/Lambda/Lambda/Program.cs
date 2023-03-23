59ab9523a5a89fa5ae362cdeef08b39b052b4f42
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2023-03-23 14:26:27 -0300

    add files

---
+++ CSharp/Lambda/Lambda/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+       
+     var lista = new List<int>() { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+        //var listaFiltrada = lista.Where(Filtro); //Passando função como referencia, e não executando ela: Filtro()
+        var listaFiltrada = lista.Where(x => x > 4);
+
+        foreach (var x in listaFiltrada)
+        {
+            Console.WriteLine(x);
+        }
+    }
+
+    public bool Filtro(int x)
+    { 
+        return x > 4;
+    }
+
+}
